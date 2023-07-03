eaf8be5db5c81236d00b8742a4a685082d407d76
BrunoM1234 <101809271+BunoM1234@users.noreply.github.com>
2023-07-03 15:49:43 -0300

    Prisma-PlanetScale Connection Base

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.get("/api", (req, res) => {
+  res.json({ message: "Hola desde el servidor!" });
 });
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto: ${PORT}`);
+});
+
+require('dotenv').config()
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
+connection.end()
