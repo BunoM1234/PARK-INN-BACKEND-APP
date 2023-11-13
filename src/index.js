9eec76bca34976885a7857471043d4ef03b1e5e2
BrunoM1234 <101809271+BunoM1234@users.noreply.github.com>
2023-11-13 16:00:53 -0300

    avance falta create

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+
+
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hola desde el servidor!" });
 });
@@ -50,25 +53,23 @@ app.post('/loginValidation', async (req, res) => {
     res.json(userLog);
   }
   else {
-    res.send({ msg: "No existe el usuario" })
+    res.status(404).send("No existe el usuario");
   }
-})
+});
+
+
 
 app.post('/CreateParking', async (req, res) => {
-  const { adress, type, capacity, userID } = req.body;
+  const { adress, type, capacity, contact } = req.body;
   try {
     const add = await prisma.Estacionamientos.create({
       data: {
         adress,
         type,
         capacity,
-        usuario: {
-          connect: {
-            ID: userID  
-          }
-        }
+        contact,
       }
-    });
+      });
     res.json(add);
   } catch (error) {
     console.error('Error al crear el Parking:', error);
@@ -84,3 +85,4 @@ app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
